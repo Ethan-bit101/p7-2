117b645a759ac47114a487bccf07fd91ef03cb37
Ethan <136395115+Ethan-bit101@users.noreply.github.com>
2025-12-05 20:09:55 +0700

    Update page.tsx

--- src/app/Multo/page.tsx
+++ src/app/Multo/page.tsx
@@ -10,7 +10,7 @@ import { AlertCircleIcon, BadgeCheckIcon, CheckIcon } from "lucide-react"
 import { Sheet,
          SheetClose,
          SheetContent,
-         SheetDesription,
+         SheetDescription,
          SheetFooter,
          SheetHeader,
          SheetTitle,
@@ -346,4 +346,5 @@ interface JwtPayload {
     </div>
   );
 }
+
 
